79c39b9205049daa6b7b5cdd0e103e5f8fb87225
amdjfsonb81 <amdjfsonb81@Amdsepjfs272.iiht.tech>
2022-09-23 12:30:38 +0530

    first

--- Devops_basics/src/Devpak/Simple.java
+++ Devops_basics/src/Devpak/Simple.java
@@ -1,11 +1,21 @@
 package Devpak;
 
+import java.util.*;
+
 public class Simple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello word");
-
+      
+         Scanner s=new Scanner(System.in);
+         System.out.println("Enter number");
+         int n=s.nextInt();
+         for(int i=1;i<=10;i++)
+         {
+        	
+        	System.out.println(n+"*"+i+"="+n*i);
+         }
 	}
 
 }
